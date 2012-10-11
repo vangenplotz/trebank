75771e6a84903d3cacbb69fbf6fa12aec7a4e279
Paalmessenlien <paal@vangenplotz.no>
2012-10-11 09:41:03 +0200

    Label visninger

--- content-listing.php
+++ content-listing.php
@@ -9,10 +9,10 @@
 <?php the_listing_thumbnail(); ?>
 <?php the_listing_fields(); ?>
 <?php $website = get_post_meta( get_the_ID(), 'website', true ); ?>
-<p class="address">Adresse: <?php the_listing_address(); ?></p>
-<p class="phone">Telefon:<strong><?php echo esc_html( get_post_meta( get_the_ID(), 'phone', true ) ); ?></strong></p>
+<p class="address"><span class="custom-field-label">Adresse</span>: <?php the_listing_address(); ?></p>
+<p class="phone"><span class="custom-field-label">Telefon</span>: <strong><?php echo esc_html( get_post_meta( get_the_ID(), 'phone', true ) ); ?></strong></p>
 <?php if ( $website ) : ?>
-		<p class="listing-website">Web:<a href="<?php echo esc_url( 'http://' . $website ); ?>" title="<?php _e( 'Website', APP_TD ); ?>" target="_blank"><?php echo esc_html( $website ); ?></a></p>
+		<p class="listing-website"><span class="custom-field-label">Web</span>: <a href="<?php echo esc_url( 'http://' . $website ); ?>" title="<?php _e( 'Website', APP_TD ); ?>" target="_blank"><?php echo esc_html( $website ); ?></a></p>
 <?php endif; ?>
-<p class="more-info"><?php echo html_link( get_permalink(), __( 'Mer informasjon', APP_TD ) ); ?></p>
+<p class="more-info"><a href="<?php the_permalink(); ?>" rel="bookmark">Mer informasjon</a></p>
 </div>
