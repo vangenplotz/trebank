6d6e30f97543b11654b3653e82ff7a263b56c454
Pål Messenlien <messenlien@gmail.com>
2012-11-14 08:39:26 +0100

    Frontpage

--- frontpage.php
+++ frontpage.php
@@ -2,13 +2,10 @@
 /*
 Template Name: Frontpage
 */
-get_header(); ?>
-		<div id="primary">
+?>
+<div id="primary">
 			<div id="content" role="main">
 				<?php dynamic_sidebar( 'header' ); ?>
-				<?php while ( have_posts() ) : the_post(); ?>
 					<?php get_template_part( 'content', 'page' ); ?>
-				<?php endwhile; // end of the loop. ?>
-
 			</div><!-- #content -->
 </div><!-- #primary -->
