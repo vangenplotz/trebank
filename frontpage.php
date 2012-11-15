7fc434dadae1adcd4a2be2e33ba64a65153d7845
Pål Messenlien <messenlien@gmail.com>
2012-11-15 08:29:27 +0100

    Strip page

--- frontpage.php
+++ frontpage.php
@@ -4,27 +4,13 @@ Template Name: Frontpage
 */
 ?>
 <div id="main">	
-
-	<?php appthemes_before_page_loop(); ?>
-
-	<?php while ( have_posts() ) : the_post(); ?>
-
-	<?php appthemes_before_page(); ?>
-
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-		<?php appthemes_before_page_title(); ?>
-
-		<header>
-			<h1><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
-		</header>
-
-		<?php appthemes_after_page_title(); ?>
 
 		<section id="overview">
 
 			<?php appthemes_before_page_content(); ?>
-
+			<?php dynamic_sidebar( 'header' ); ?>
 			<?php the_content(); ?>
 
 			<?php appthemes_after_page_content(); ?>
@@ -33,14 +19,4 @@ Template Name: Frontpage
 	<?php edit_post_link( __( 'Edit', APP_TD ), '<span class="edit-link">', '</span>' ); ?>
 	</article>
 
-	<?php appthemes_after_page(); ?>
-
-	<?php endwhile; ?>
-
-	<?php appthemes_after_page_loop(); ?>
-
 </div><!-- /#main -->
-
-<div id="sidebar" class="threecol last">
-	<?php get_sidebar( app_template_base() ); ?>
-</div>	
