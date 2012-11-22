348795974b7c63e57efb18a6afd3b279298cace5
Kristian Skramstad <kristian@vangenplotz.no>
2012-11-22 16:09:14 +0100

    la til ny logo, fiksa css og la til ny bakgrunn.

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <div id="footer" class="container">
 	<div class="row">
-	<p class="copyright">Copyright © 2012 Trebank</p> <p class="made-by">Design: <a href="http://www.reklamare.no" target="_blank">Reklamare</a> - Utvikling av: <a href="http://vangenplotz.no" alt="Utviklet av Vangen &amp; Plotz AS" title="Utviklet av Vangen &amp; Plotz AS"><img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/images/vp-logo.png" width="22" height="22" alt="Utviklet av Vangen &amp; Plotz AS"></a>
+	<p class="copyright">Copyright 2012 Trebank</p> <p class="made-by">Design: <a href="http://www.reklamare.no" target="_blank">Reklamare</a> - Utvikling av: <a href="http://vangenplotz.no" alt="Utviklet av Vangen &amp; Plotz AS" title="Utviklet av Vangen &amp; Plotz AS"><img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/images/vp-logo.png" width="22" height="22" alt="Utviklet av Vangen &amp; Plotz AS"></a>
 		<?php dynamic_sidebar( 'va-footer' ); ?>
 	</div>
 </div>
--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <div id="masthead">
 		<hgroup>
-			<a href="/"><img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/images/logo-trebank.png" \></a>
+			<a href="/"><img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/images/trebank_logo.png" \></a>
 		</hgroup>
 </div>
 <div class="green">
@@ -9,7 +9,7 @@
         <label for="search">S&Oslash;K  </label>
         <label class="info">(f.eks overflatebehandling, limtredrager, h&oslash;vleri)</label>
         <input class="text" type="text" name="s" id="search" value="<?php the_search_query(); ?>" />
-        <input class="sbutton" type="image" alt="Search" src="<?php bloginfo( 'stylesheet_directory' ); ?>/images/search-blank.png" />
+        <input class="sbutton" type="image" alt="Search" src="<?php bloginfo( 'stylesheet_directory' ); ?>/images/sok_blankt.png" />
     </fieldset>
 </form>
 </div>
