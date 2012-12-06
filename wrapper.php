28b2a77551c4c2d91b1c9af94871fbae237595a8
Kristian Skramstad <kristian@vangenplotz.no>
2012-12-06 11:02:09 +0100

    Endret til drop down meny.

--- wrapper.php
+++ wrapper.php
@@ -16,9 +16,16 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 	<?php wp_head(); ?>
-
+	<script> 
+		jQuery(document).ready(function ($) {
+		$(".tjenester").click(function(){
+	    $(".tags_listing").slideToggle('slow');
+	  });
+	});
+	</script>
 	<!--[if lte IE 9]><link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/styles/ie.css" type="text/css" media="screen" /><![endif]-->
 	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+
 </head>
 
 <body <?php body_class(); ?>>
@@ -31,6 +38,19 @@
 	<?php appthemes_after_header(); ?>
 		<?php wp_nav_menu( array( 'theme_location' => 'sidebar-menu' ) ); ?>
 		<?php dynamic_sidebar( 'main' ); ?>
+		
+		<a href="#"><div class="tjenester">Tjenester</div></a>
+		<div class="tags_listing">
+			<?php 
+			  $args = array(
+			    'taxonomy'  => array('post_tag','listing_tag'), 
+			   ); 
+			   
+			   wp_tag_cloud($args);
+			   ?>
+		</div>
+
+
 	</div>
 	<div id="content">
 				<?php include app_template_path(); ?>
